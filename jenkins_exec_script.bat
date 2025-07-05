19b6d784f00820b72b66d2560b9bb5bd5afa63f0
SAURABH SINGH <72145932+saurabhkingp@users.noreply.github.com>
2025-07-06 03:59:09 +0530

    improve jenkins script output formatting

--- jenkins_exec_script.bat
+++ jenkins_exec_script.bat
@@ -1,7 +1,7 @@
 COLOR 0A
-ECHO === Activating Virtual Environment ===
+ECHO [32;40m=== Activating Virtual Environment 
 CALL c:/Users/saura/OneDrive/Desktop/open_source_venv/Scripts/activate.bat
-ECHO === Running YouTube Data Script ===
+ECHO Running YouTube Data Script 
 python youtube.py
-ECHO === Checking Python Version ===
-python --version
+ECHO Checking Python Version 
+python --version ===[0m
