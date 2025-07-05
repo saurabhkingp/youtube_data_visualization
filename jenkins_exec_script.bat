a3f4fc0ea9d10d379e1cb7050753a621473e0594
SAURABH SINGH <72145932+saurabhkingp@users.noreply.github.com>
2025-07-06 03:40:44 +0530

    fixed

--- jenkins_exec_script.bat
+++ jenkins_exec_script.bat
@@ -1,3 +1,6 @@
-CALL c:/Users/saura/OneDrive/Desktop/open_source_venv/Scripts/activate.bat 
+ECHO === Activating Virtual Environment ===
+CALL c:/Users/saura/OneDrive/Desktop/open_source_venv/Scripts/activate.bat
+ECHO === Running YouTube Data Script ===
 python youtube.py
+ECHO === Checking Python Version ===
 python --version
